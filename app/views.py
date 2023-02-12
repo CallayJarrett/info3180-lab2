4a8130b9f0eb8dbfe08d427d241510a40e56f33e
Callay Jarrett <jcallay@yahoo.com>
2023-02-11 22:55:51 -0500

    lab 2 complete

--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
-
+import datetime
 
 ###
 # Routing for your application.
 ###
+
+def format_date_joined(y,m,d):
+    now = datetime.datetime.now() # today's date
+    date_joined = datetime.date(y,m,d) # a specific date 
+    return date_joined.strftime("%B, %Y")
+
 
 @app.route('/')
 def home():
@@ -16,6 +22,10 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html',joined=format_date_joined(2023,2,11))
 
 
 ###
